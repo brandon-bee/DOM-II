b4351e221ef4e2cda8e475f1142a542769f9920a
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2021-11-30 13:18:05 -0800

    3/10 event listeners setup and functioning properly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// 1. Mouseover event, color change on logo
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', evt => {
+  evt.target.style.color = 'orange';
+  setTimeout(() => {
+    evt.target.style.color = '';
+  }, 500);
+})
+
+// 2. Keydown event, change H2 color to pink
+const titles = document.querySelectorAll('h2');
+document.addEventListener('keydown', evt => {
+  if(evt.key === 'p') {
+    titles.forEach(title => title.style.color = 'pink');
+  }
+})
+
+// 3. Click event, with preventDefault on links to open to RickRoll links
+const links = document.querySelectorAll('.nav-link');
+links.forEach(link => {
+  link.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'https://rickrolled.com/');
+  })
+})
+
+// 4. 
+
+// 5. 
+
+// 6. 
+
+// 7. 
+
+// 8. 
+
+// 9. 
+
+// 10. 
